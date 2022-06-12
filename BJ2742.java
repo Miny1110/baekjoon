b5cd1d6e5a071fc07c3eb066c69baebc256b42f7
Miny1110 <wjdalswjd453@naver.com>
2022-06-12 12:42:59 +0900

    Create BJ2742.java

---
+++ BJ2742.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BJ2742 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n, i;
+		
+		n = sc.nextInt();
+		
+		for(i=n;i>=1;i--) {
+			System.out.println(i);
+		}
+	}
+}
